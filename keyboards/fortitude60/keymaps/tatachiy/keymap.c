f734c76d8fbdf47e87cf85fa224076bef62f5263
Takehiro TACHIKAWA <TakehiroTACHIKAWA@gmail.com>
2019-02-24 02:29:10 +0900

    キーマップ変更

--- keyboards/fortitude60/keymaps/tatachiy/keymap.c
+++ keyboards/fortitude60/keymaps/tatachiy/keymap.c
@@ -35,11 +35,14 @@ enum custom_keycodes {
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define LOWER_L LT(_LOWER, KC_MHEN)	// タップで無変換     ホールドでLower
-#define LOWER_R LT(_LOWER, KC_HENK)	// タップで変換       ホールドでRaise
+#define LOWER_L LT(_LOWER, KC_ENT)
+#define LOWER_R LT(_LOWER, KC_ENT)
 #define RAISE_L LT(_RAISE, KC_SPC)
 #define RAISE_R LT(_RAISE, KC_SPC)
-#define CTL_ENT CTL_T(KC_ENT)
+#define CTL_L CTL_T(KC_MHEN)
+#define CTL_R CTL_T(KC_HENK)
+#define CTL_ML GUI_T(JP_MEISU)
+#define CTL_MR GUI_T(JP_MKANA)
 #define EMACS MO(_LOWER)          // EMacs風
 #define SPC_ADJ LT(_ADJUST, KC_SPC)
 
@@ -49,8 +52,8 @@ enum custom_keycodes {
 #define ALT_KANA ALT_T(JP_MKANA)
 #define ALT_EISU ALT_T(JP_MEISU)
 
-#define LOWER_ML LT(_MAC_LOWER, JP_MEISU)	// タップで無変換     ホールドでLower
-#define LOWER_MR LT(_MAC_LOWER, JP_MKANA)	// タップで変換       ホールドでRaise
+#define LOWER_ML LT(_MAC_LOWER, KC_ENT)	// タップで無変換     ホールドでLower
+#define LOWER_MR LT(_MAC_LOWER, KC_ENT)	// タップで変換       ホールドでRaise
 
 #define KC_L1 KC_LGUI
 #define KC_L2 KC_LALT
@@ -78,15 +81,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  | Bksp |  Del |   N  |   M  |   ,  |   .  |   /  |BSL/Sf|
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  GUI |  Alt | Enter| Lower| Raise|Raise |Lower | Enter|  Alt |  GUI |
+ *               |  GUI |  Alt | Ctrl | Lower| Raise|Raise |Lower | Ctrl |  Alt |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
   KC_LCTRL,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC,  KC_BSPC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_BSLS, \
-                    KC_LGUI, KC_LALT, CTL_ENT, LOWER_L,RAISE_L,  RAISE_R, LOWER_R, CTL_ENT, KC_RALT, KC_RGUI\
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC,  KC_BSPC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_T(JP_BSLS), \
+                    KC_LGUI, KC_LALT, CTL_L,   LOWER_L,RAISE_L,  RAISE_R, LOWER_R, CTL_R,   KC_RALT, KC_RGUI\
 ),
 
 /* Mac
@@ -106,49 +109,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
     KC_LCTRL,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC, KC_BSPC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_UNDS, \
-                      KC_LGUI, KC_LALT, CTL_ENT, LOWER_ML,RAISE_L, RAISE_L, LOWER_MR,CTL_ENT, KC_RALT, KC_RGUI\
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC, KC_BSPC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_T(JP_BSLS), \
+                      KC_LGUI, KC_LALT, CTL_ML,  LOWER_ML,RAISE_L, RAISE_L, LOWER_MR,CTL_MR,  KC_RALT, KC_RGUI\
     ),
 
 /* Lower
  * ,-----------------------------------------.             ,-----------------------------------------.
- * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |   =  |
+ * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |   ^  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |CtlTab|  F11 |  F12 |  End | CtrlR|   T  |             | CtrlY|   U  |   I  | EntO |   ↑  |   `  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Ctrl | Home | CtrlS|  Del |   →  | CtrlG|             |  BS  |   J  | DelK |   L  |   ;  |   *  |
+ * | Ctrl | Home | CtrlS|  Del |   →  | CtrlG|             |  BS  |   J  | DelK |   L  |   +  |   *  |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift| CtrlZ| CtrlX| CtrlC| CtrlV|   ←  | Bksp |  Del |   ↓  |   M  | Ctrl,| Ctrl.|   /  |   _ |
+ * | Shift| CtrlZ| CtrlX| CtrlC| CtrlV|   ←  | Bksp |  Del |   ↓  |   M  | Ctrl,| Ctrl.|  ?  |   _  |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |  GUI |  Alt | Enter| Lower| Raise|Raise |Lower | Enter|  Alt |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______,      KC_F1,      KC_F2,      KC_F3,      KC_F4,       KC_F5,                       KC_F6,       KC_F7,   KC_F8,         KC_F9,   KC_F10,  JP_EQL, \
-  LCTL(KC_TAB), KC_F11,     KC_F12,     KC_END,     LCTL(KC_R),  LCTL(KC_T),                  LCTL(KC_Y),  XXXXXXX, XXXXXXX,       MC_ENTO, KC_UP,   JP_GRV, \
-  _______,      KC_HOME,    LCTL(KC_S), KC_DEL,     KC_RGHT,     LCTL(KC_G),                  KC_BSPC,     XXXXXXX, MC_DELK,       XXXXXXX, JP_SCLN, JP_ASTR, \
-  _______,      LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  KC_LEFT,  KC_DEL,   KC_DEL,  KC_DOWN,     XXXXXXX, LCTL(KC_COMMA),LCTL(KC_DOT),JP_SLSH, JP_UNDS, \
+  JP_GRV,       KC_F1,      KC_F2,      KC_F3,      KC_F4,       KC_F5,                       KC_F6,       KC_F7,   KC_F8,         KC_F9,   KC_F10,  JP_MINS, \
+  LCTL(KC_TAB), KC_F11,     KC_F12,     KC_END,     LCTL(KC_R),  LCTL(KC_T),                  LCTL(KC_Y),  XXXXXXX, XXXXXXX,       MC_ENTO, KC_UP,   JP_LBRC, \
+  _______,      KC_HOME,    LCTL(KC_S), KC_DEL,     KC_RGHT,     LCTL(KC_G),                  KC_BSPC,     XXXXXXX, MC_DELK,       XXXXXXX, JP_SCLN, JP_QUOT, \
+  _______,      LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  KC_LEFT,  KC_DEL,   KC_DEL,  KC_DOWN,     XXXXXXX, LCTL(KC_COMMA),LCTL(KC_DOT),JP_SLSH, JP_YEN, \
                             _______,    _______,    _______,     _______,  _______,  _______, _______,     _______, _______,       _______\
 ),
 
 /* Raise
  * ,-----------------------------------------.             ,-----------------------------------------.
- * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |            |   &  |   '  |   (  |   )  |   ^  |   !  |
+ * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |             |   ^  |   ¥  |   (  |   )  |   ^  |   ¥  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Tab  |  F6  |  F7  |  F8  |  F9  |  F10 |             |   ¥  |   |  |   [  |   ]  | PgUp |   "  |
+ * | Tab  |  F6  |  F7  |  F8  |  F9  |  F10 |             |   ~  |   |  |   [  |   ]  | PgUp |      |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Ctrl | F11  |  F12 |CtlDel| Ctrl→| CtrlG|             | CtlBS|   ~  |   {  |   }  |   +  |   #  |
+ * | Ctrl | F11  |  F12 |CtlDel| Ctrl→| CtrlG|             | CtlBS|      |   {  |   }  |      |      |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift| CtrlZ| CtrlX| CtrlC| CtrlV| Ctrl←| Del | Bksp | PgDn |   %  |   <  |   >  |   ?  |   $  |
+ * | Shift| CtrlZ| CtrlX| CtrlC| CtrlV| Ctrl←| Del | Bksp | PgDn |      |   <  |   >  |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |  GUI |  Alt | Enter| Lower| Raise|Raise |Lower | Enter|  Alt |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
-  _______,   KC_F1,      KC_F2,      KC_F3,      KC_F4,     KC_F5,                       JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_CIRC, JP_EXLM, \
-  KC_TAB,    KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,                      JP_YEN,  JP_PIPE, JP_LBRC, JP_RBRC, KC_PGUP, JP_DQT, \
-  _______,   KC_F11,     KC_F12,     MC_DEL,     MC_CRGHT,  LCTL(KC_G),                  MC_BCSP, JP_TILD, JP_LCBR, JP_RCBR, JP_PLUS, JP_HASH, \
-  _______,   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),MC_CLEFT, MC_BCSP,  MC_BCSP, KC_PGDN, JP_PERC, JP_LT,   JP_GT,   JP_QUES, JP_DLR, \
+  JP_TILD,   KC_F1,      KC_F2,      KC_F3,      KC_F4,     KC_F5,                       JP_CIRC, JP_YEN,  JP_LPRN, JP_RPRN, JP_CIRC, JP_UNDS, \
+  KC_TAB,    KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,                      JP_TILD, JP_PIPE, JP_LBRC, JP_RBRC, KC_PGUP, JP_LCBR, \
+  _______,   KC_F11,     KC_F12,     MC_DEL,     MC_CRGHT,  XXXXXXX,                     MC_BCSP, XXXXXXX, JP_LCBR, JP_RCBR, JP_COLN, JP_DQT, \
+  _______,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   MC_CLEFT, MC_BCSP,  MC_BCSP, KC_PGDN, XXXXXXX, JP_LT,   JP_GT,   JP_QUES, JP_PIPE, \
                           _______,    _______,    _______,  _______,  _______,  _______, _______, _______, _______, _______\
 ),
 
@@ -166,19 +169,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QWERTY, \
-  KC_ACL0, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC, \
-  KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, KC_RGHT, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  KC_ACL2, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,      RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                    KC_F11,  KC_F12,  _______, _______, _______,  _______, _______, _______, _______, _______\
+  C(S(KC_ESC)),XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, KC_ACL0,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PENT, LCA(KC_DEL), \
+  XXXXXXX,     KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, KC_ACL1,           XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR, MAC, \
+  KC_WH_L,     KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, KC_ACL2,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, XXXXXXX, \
+  XXXXXXX,     XXXXXXX, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX,  RESET,   RESET,   QWERTY,  MAC,     XXXXXXX, XXXXXXX, KC_PAUS, XXXXXXX, \
+                        XXXXXXX, XXXXXXX, _______, _______, _______,  _______, _______, _______, _______, _______\
 ),
 
 /* Lower_Mac */
 [_MAC_LOWER] = LAYOUT( \
-  KC_ESC,       KC_F1,      KC_F2,       KC_F3,      KC_F4,      KC_F5,                       KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  JP_EQL, \
-  LCTL(KC_TAB), KC_F11,     KC_F12,      LCTL(KC_E), LGUI(KC_R), LGUI(KC_T),                  LGUI(LSFT(KC_Y)),  XXXXXXX, XXXXXXX, MC_ENTO, KC_UP,   JP_GRV, \
-  _______,      LCTL(KC_A),LGUI(KC_S),   KC_DEL,     KC_RGHT,    LGUI(KC_G),                  KC_BSPC,           XXXXXXX, MC_DELK, KC_L,    JP_SCLN, JP_ASTR, \
-  _______,      LGUI(KC_Z), LGUI(KC_X),  LGUI(KC_C), LGUI(KC_V), KC_LEFT,  _______,  _______, KC_DOWN,           XXXXXXX, KC_COMMA,KC_DOT,  JP_SLSH, JP_UNDS, \
+  JP_GRV,       KC_F1,      KC_F2,       KC_F3,      KC_F4,      KC_F5,                       KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  JP_MINS, \
+  LCTL(KC_TAB), KC_F11,     KC_F12,      LCTL(KC_E), LGUI(KC_R), LGUI(KC_T),                  LGUI(LSFT(KC_Y)),  XXXXXXX, XXXXXXX, MC_ENTO, KC_UP,   JP_LBRC, \
+  _______,      LCTL(KC_A),LGUI(KC_S),   KC_DEL,     KC_RGHT,    LGUI(KC_G),                  KC_BSPC,           XXXXXXX, MC_DELK, XXXXXXX, JP_SCLN, JP_QUOT, \
+  _______,      LGUI(KC_Z), LGUI(KC_X),  LGUI(KC_C), LGUI(KC_V), KC_LEFT,  _______,  _______, KC_DOWN,           XXXXXXX, KC_COMMA,KC_DOT,  JP_SLSH, JP_YEN, \
                             _______,     _______,    _______,    _______,  _______,  _______, _______,           _______, _______, _______\
 )
 
